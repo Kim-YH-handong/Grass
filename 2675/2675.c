1428237692a1bf1ac9b81de7852459d9770ad701
Kim-YH-handong <82792998+Kim-YH-handong@users.noreply.github.com>
2022-01-15 11:25:06 +0900

    2675

---
+++ 2675/2675.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+void printP(char *S, int R);
+
+int main(){
+    int T, R, j=0;
+    char S[20];
+
+    scanf("%d", &T);
+
+    for(int i = 0; i < T; i++){
+        j = 0;
+        scanf("%d", &R);
+        scanf("%s", S);
+        while(strcmp(&S[j], "")){
+            for(int x = 0; x < R; x++){
+                printf("%c", S[j]);
+            }
+            j++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
